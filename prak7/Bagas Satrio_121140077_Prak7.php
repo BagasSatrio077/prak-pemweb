671fb96a92a74ca43c1717f366c204e3fcaab3d8
BagasSatrio077 <126631952+BagasSatrio077@users.noreply.github.com>
2023-11-24 09:54:12 +0700

    Create Bagas Satrio_121140077_Prak7.php

---
+++ prak7/Bagas Satrio_121140077_Prak7.php
@@ -0,0 +1,31 @@
+<?php
+//untuk membuat class DataDiri
+class DataDiri {
+  //untuk membuat property nama, alamat, dan email
+  public $nama;
+  public $alamat;
+  public $email;
+
+  //untuk membuat constructor untuk menginisialisasi property
+  public function __construct($nama, $alamat, $email) {
+    $this->nama = $nama;
+    $this->alamat = $alamat;
+    $this->email = $email;
+  }
+
+  //untuk membuat method untuk menampilkan data diri
+  public function tampilData() {
+    echo "Nama: " . $this->nama . "<br>";
+    echo "Alamat: " . $this->alamat . "<br>";
+    echo "Email: " . $this->email . "<br>";
+  }
+}
+
+//untuk membuat objek dari class DataDiri
+$data1 = new DataDiri("Bagas Satrio", "Kemiling", "bagas@gmail.com");
+$data2 = new DataDiri("Anibas Larisa", "Way Halim", "anibas@gmail.com");
+
+//untuk memanggil method tampilData dari objek
+$data1->tampilData();
+$data2->tampilData();
+?>
